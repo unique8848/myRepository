97823e2443538d952f855f40f74ace85e11441be
Kevin <unique8848@126.com>
2015-01-05 00:17:19 +0800

    refactor

--- kf/src/com/ktw/kf/dao/BaseDao.java
+++ kf/src/com/ktw/kf/dao/BaseDao.java
@@ -1,6 +1,6 @@
 package com.ktw.kf.dao;
 
-import com.ktw.kf.db.ConnectorFactory;
+import com.ktw.kf.db.ConnectionManager;
 
 import java.sql.Connection;
 
@@ -8,5 +8,5 @@ import java.sql.Connection;
  * Created by kevin on 12/30/14.
  */
 public abstract class BaseDao {
-    protected static Connection conn = ConnectorFactory.getMysqlConnector().getConnection();
+    protected static Connection conn = ConnectionManager.getConnection();
 }
